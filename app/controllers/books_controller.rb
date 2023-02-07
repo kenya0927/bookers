18302053376f9215d35618ae0882663f71f9d715
kenya0927 <123355087+kenya0927@users.noreply.github.com>
2023-02-07 18:16:50 +0000

    [Update]3-13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,25 @@ class BooksController < ApplicationController
   end
 
   def index
+     @book =Book.new
   end
 
   def show
   end
 
   def edit
+  end
+  
+  def cretate
+    book = Book.new(book_params)
+    
+    book.save
+  end
+  
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
